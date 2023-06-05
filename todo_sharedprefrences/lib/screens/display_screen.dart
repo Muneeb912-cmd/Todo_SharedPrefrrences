bbe2235d81621ce047626a983ec4ae4fdf16ed0f
Muneeb912-cmd <81864431+Muneeb912-cmd@users.noreply.github.com>
2023-06-05 11:41:08 +0500

    commit1

---
+++ todo_sharedprefrences/lib/screens/display_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class display_screen extends StatefulWidget {
+  const display_screen({super.key});
+
+  @override
+  State<display_screen> createState() => _display_screenState();
+}
+
+class _display_screenState extends State<display_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
